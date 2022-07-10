d8b5fba01bfd9e301287478a06724cf95cc9d005
Suraj Pillai <suraj.pillai@vertexcs.com>
2022-07-10 01:51:59 -0400

    Add LWCs for file upload in memory and in SF

---
+++ force-app/main/default/lwc/fileUploadLocal/fileUploadLocal.js
@@ -0,0 +1,87 @@
+import { LightningElement, api } from "lwc";
+import { FlowAttributeChangeEvent } from "lightning/flowSupport";
+
+export default class FileUploadLocal extends LightningElement {
+  _fileContents = "";
+  _fileName = "";
+  errMsg = "";
+
+  @api
+  useLocalStorage = false;
+
+  _fileId = "";
+
+  @api
+  get fileId() {
+    return this._fileId;
+  }
+
+  @api
+  get fileUId() {
+    return this._fileId;
+  }
+
+  @api
+  get fileContents() {
+    return this._fileContents;
+  }
+
+  @api
+  get fileName() {
+    return this._fileName;
+  }
+
+  @api
+  availableActions = [];
+
+  _handleUsingString = (evt) => {
+    this._fileContents = btoa(evt.target.result);
+    const attrChangeEvent = new FlowAttributeChangeEvent(
+      "fileContents",
+      this.fileContents
+    );
+    this.dispatchEvent(attrChangeEvent);
+  };
+
+  _handleUsingLocalStorage = (evt) => {
+    const uuid = "file-" + ~~(Math.random() * 10000);
+    try {
+      localStorage.setItem(uuid, evt.target.result);
+    } catch (err) {
+      console.error("saving file to local storage failed", err);
+      return;
+    }
+    this._fileId = uuid;
+    const attrChangeEvent = new FlowAttributeChangeEvent("fileId", this.fileId);
+    console.log("attribute change event fired for file id " + this.fileId);
+    this.dispatchEvent(attrChangeEvent);
+  };
+
+  handleUpload(evt) {
+    this.errMsg = "";
+    let file = evt.target.files[0];
+    if (!this.validateSize(file.size)) {
+      this.template.querySelector("input.file").value = null;
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = this.useLocalStorage
+      ? this._handleUsingLocalStorage
+      : this._handleUsingString;
+    reader.readAsBinaryString(evt.target.files[0]);
+    this._fileName = evt.target.files[0].name;
+    const attrChangeEvent = new FlowAttributeChangeEvent(
+      "fileName",
+      this.fileContents
+    );
+    this.dispatchEvent(attrChangeEvent);
+  }
+
+  validateSize(size) {
+    if ((size / 1024 / 1024).toFixed(1) > 2.2) {
+      this.errMsg = "Only files upto 2MB in size are supported";
+      return false;
+    }
+    return true;
+  }
+}
---
+++ force-app/main/default/lwc/fileUploadSF/fileUploadSF.js
@@ -0,0 +1,69 @@
+import { LightningElement, api } from "lwc";
+import { FlowNavigationNextEvent } from "lightning/flowSupport";
+import { createRecord } from "lightning/uiRecordApi";
+
+export default class FileUploadSF extends LightningElement {
+  @api
+  title = "FlowFileUpload.txt";
+
+  @api
+  fileContents;
+
+  errMsg = "";
+
+  @api
+  fileId;
+
+  @api
+  recordId;
+
+  @api
+  availableActions = [];
+
+  processing = false;
+
+  async connectedCallback() {
+    await this.createCv();
+  }
+
+  clearLocalStorage() {
+    if (this.fileId) {
+      localStorage.removeItem(this.fileId);
+    }
+  }
+
+  async createCv() {
+    try {
+      const fileData = {
+        Title: this.title,
+        PathOnClient: this.title,
+        VersionData: this.fileId
+          ? window.btoa(localStorage.getItem(this.fileId))
+          : this.fileContents,
+        Description: "Uploaded from Flow File Uploader"
+      };
+      if (this.recordId) fileData.FirstPublishLocationId = this.recordId;
+      this.clearLocalStorage();
+      const payload = { apiName: "ContentVersion", fields: fileData };
+      this.processing = true;
+      const resp = await createRecord(payload);
+      if (resp.error) {
+        throw Error(resp.error.body.message);
+      }
+    } catch (err) {
+      console.error("An error occured ", err);
+      this.errMsg = err.message || err.body.message;
+    } finally {
+      this.processing = false;
+    }
+  }
+
+  handleGoNext() {
+    // check if NEXT is allowed on this screen
+    if (this.availableActions.find((action) => action === "NEXT")) {
+      // navigate to the next screen
+      const navigateNextEvent = new FlowNavigationNextEvent();
+      this.dispatchEvent(navigateNextEvent);
+    }
+  }
+}
